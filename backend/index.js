ffb63ec6de7a96c13a6a5b22af2e5207d5abb2bd
eajajul <eajajul@gmail.com>
2022-08-18 20:42:47 +0600

    wip: db and server setup

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,27 @@
-import app from "./server.js"
-import mongodb from "mongodb"
-import dotenv from "dotenv"
+import app from './server.js';
+import mongodb from 'mongodb';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const MongoClient = mongodb.MongoClient
-const port = process.env.PORT || 8000
+const MongoClient = mongodb.MongoClient;
+const port = process.env.PORT || 8000;
 
-const uri = "mongodb+srv://starlight404: mern123@project.ichqunk.mongodb.net/?retryWrites=true&w=majority";
+const uri =process.env.RESREV_DB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
-client.connect()
-
-.catch(err => {
-    console.error(err.stack)
-    process.exit(1)
-})
-.then(async client => {
-    app.listen(port, () =>{
-        console.log(`listening on port ${port}`)
-    })
-})
+// FIXME:Code refactoring
+client.connect((err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected to database');
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+  }
+  client.close();
+});
